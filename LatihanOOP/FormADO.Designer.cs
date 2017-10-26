0d7bd7493c93e9bdae78aa97db145fc070fcbdf2
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-26 14:39:35 +0700

    menambahkan connection string

---
+++ LatihanOOP/FormADO.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LatihanOOP
+{
+    partial class FormADO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKonek = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKonek
+            // 
+            this.btnKonek.Location = new System.Drawing.Point(12, 12);
+            this.btnKonek.Name = "btnKonek";
+            this.btnKonek.Size = new System.Drawing.Size(75, 23);
+            this.btnKonek.TabIndex = 0;
+            this.btnKonek.Text = "Koneksi";
+            this.btnKonek.UseVisualStyleBackColor = true;
+            this.btnKonek.Click += new System.EventHandler(this.btnKonek_Click);
+            // 
+            // FormADO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnKonek);
+            this.Name = "FormADO";
+            this.Text = "FormADO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKonek;
+    }
+}
---
+++ LatihanOOP/FormADO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace LatihanOOP
+{
+    public partial class FormADO : Form
+    {
+        //membuat object koneksi
+        private MySqlConnection conn;
+        private MySqlCommand cmd;
+
+        public FormADO()
+        {
+            InitializeComponent();
+            string strConn = "Server=localhost;Database=posdb;Uid=root;Pwd=;";
+            conn = new MySqlConnection(strConn);
+        }
+
+        private void btnKonek_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Koneksi berhasil dibuka !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
