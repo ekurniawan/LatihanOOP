f1bf7ce91167291ab72a0c687048749a23d8f998
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-27 16:45:53 +0700

    menambahkan web api

---
+++ WebAPISample/Controllers/PelangganController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BO;
+using BL;
+
+namespace WebAPISample.Controllers
+{
+    public class PelangganController : ApiController
+    {
+        private PelangganBL pelangganBL;
+
+        public PelangganController()
+        {
+            pelangganBL = new PelangganBL();
+        }
+
+        // GET: api/Pelanggan
+        public IEnumerable<Pelanggan> Get()
+        {
+            var results = pelangganBL.GetAll();
+            return results;
+        }
+
+        // GET: api/Pelanggan/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Pelanggan
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Pelanggan/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Pelanggan/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
