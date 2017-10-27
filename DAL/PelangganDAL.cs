4c4d9dc4eb72b877d3042809f48106eafe90ded3
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-27 11:11:19 +0700

    mengganti koneksi sql server

--- DAL/PelangganDAL.cs
+++ DAL/PelangganDAL.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
 using System.Configuration;
 using BO;
 
@@ -11,8 +11,16 @@ namespace DAL
 {
     public class PelangganDAL : ICrud<Pelanggan>
     {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
         private string strConn = ConfigurationManager
             .ConnectionStrings["MysqlConnectionString"].ConnectionString;
+
+        public PelangganDAL()
+        {
+            conn = new SqlConnection(strConn);
+        }
 
         public void Delete(string id)
         {
@@ -21,44 +29,42 @@ namespace DAL
 
         public IEnumerable<Pelanggan> GetAll()
         {
-            using(MySqlConnection conn = new MySqlConnection(strConn))
-            {
-                List<Pelanggan> lstPelanggan = new List<Pelanggan>();
-                string strSql = @"select * from pelanggan 
+            List<Pelanggan> lstPelanggan = new List<Pelanggan>();
+            string strSql = @"select * from pelanggan 
                                       order by Nama asc";
-                MySqlCommand cmd = new MySqlCommand(strSql, conn);
-                try
+            SqlCommand cmd = new SqlCommand(strSql, conn);
+            try
+            {
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
                 {
-                    conn.Open();
-                    MySqlDataReader dr = cmd.ExecuteReader();
-                    if (dr.HasRows)
+                    while (dr.Read())
                     {
-                        while (dr.Read())
+                        lstPelanggan.Add(new Pelanggan
                         {
-                            lstPelanggan.Add(new Pelanggan
-                            {
-                                KodePelanggan = Convert.ToInt32(dr["KodePelanggan"]),
-                                Nama = dr["Nama"].ToString(),
-                                Alamat = dr["Alamat"].ToString(),
-                                Email = dr["Email"].ToString(),
-                                Telp = dr["Telp"].ToString()
-                            });
-                        }
+                            KodePelanggan = Convert.ToInt32(dr["KodePelanggan"]),
+                            Nama = dr["Nama"].ToString(),
+                            Alamat = dr["Alamat"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            Telp = dr["Telp"].ToString()
+                        });
                     }
-                    dr.Close();
+                }
+                dr.Close();
 
-                    return lstPelanggan;
-                }
-                catch (MySqlException mysqlEx)
-                {
-                    throw new Exception(mysqlEx.Message);
-                }
-                finally
-                {
-                    cmd.Dispose();
-                    conn.Close();
-                }
+                return lstPelanggan;
             }
+            catch (SqlException mysqlEx)
+            {
+                throw new Exception(mysqlEx.Message);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+
         }
 
         public Pelanggan GetById(string id)
@@ -68,12 +74,56 @@ namespace DAL
 
         public void Insert(Pelanggan obj)
         {
-            throw new NotImplementedException();
+            string strSql = @"insert into Pelanggan(Nama,Alamat,Email,Telp) 
+                                  values(@Nama,@Alamat,@Email,@Telp)";
+            SqlCommand cmd = new SqlCommand(strSql, conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Nama", obj.Nama);
+            cmd.Parameters.AddWithValue("@Alamat", obj.Alamat);
+            cmd.Parameters.AddWithValue("@Email", obj.Email);
+            cmd.Parameters.AddWithValue("@Telp", obj.Telp);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException sqlEx)
+            {
+                throw new Exception("Error : " + sqlEx.Message);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
         }
 
         public void Update(Pelanggan obj)
         {
-            throw new NotImplementedException();
+            string strSql = @"update Pelanggan set Nama=@Nama,Alamat=@Alamat,Email=@Email,Telp=@Telp
+                              where KodePelanggan=@KodePelanggan";
+            SqlCommand cmd = new SqlCommand(strSql, conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Nama", obj.Nama);
+            cmd.Parameters.AddWithValue("@Alamat", obj.Alamat);
+            cmd.Parameters.AddWithValue("@Email", obj.Email);
+            cmd.Parameters.AddWithValue("@Telp", obj.Telp);
+            cmd.Parameters.AddWithValue("KodePelanggan", obj.KodePelanggan);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException sqlEx)
+            {
+                throw new Exception(sqlEx.Message);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
         }
     }
 }
